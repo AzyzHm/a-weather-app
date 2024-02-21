093fbda46c9166c63c585bf6f2d6900586bd5660
Azyz_Hm <128410730+C0RAZON@users.noreply.github.com>
2024-02-21 13:28:56 +0100

    added a couple of files
    added a 2 folder of images, a license and a gitignore file

---
+++ README.md
@@ -0,0 +1,71 @@
+# Sky Weather App
+
+Sky Weather is a simple and intuitive weather application that provides real-time weather information using data from [OpenWeatherMap](https://openweathermap.org/).
+
+## Table of Contents
+
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Project Status](#Project-Status)
+
+
+## Features
+
+- Display current weather conditions
+- 5-day weather forecast
+- Temperature in Celsius and Fahrenheit
+- Dynamic background based on weather conditions
+- Responsive design for various devices
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+- [OpenWeatherMap API](https://openweathermap.org/api)
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/your-username/sky-weather.git
+cd sky-weather
+```
+## Usage
+
+To use the Sky Weather app, follow these steps:
+
+1. Visit [OpenWeatherMap](https://openweathermap.org/) and sign up for a free API key.
+
+2. Replace the placeholder API key in the `script.js` file with your own API key:
+
+   ```javascript
+   const apiKey = 'your-api-key';
+
+## Screenshots
+
+In the project's root directory, you can find a folder named `screenshots` that contains PNG images:
+
+![Sky Weather App - Site Overview](/screenshots/overview.png)
+
+*Caption: Site Overview.*
+
+![Sky Weather App - Weather info of a given City](/screenshots/city-found.png)
+
+*Caption: Weather info of a given City.*
+
+![Sky Weather App - given city is not found](/screenshots/city-not-found.png)
+
+*Caption: given city is not found.*
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Project Status
+
+This project is actively maintained, and contributions are welcome. If you have any suggestions, feature requests, or bug reports, please open an issue or submit a pull request. Your input is valuable, and it will help make Sky Weather even better.
---
+++ script.js
@@ -0,0 +1,49 @@
+const givenCity = document.getElementById('search-bar');
+const searchButton = document.getElementById('search-button');
+const country = document.getElementById('country');
+const temp = document.getElementById('temp');
+const weather = document.getElementById('weather');
+const wind = document.getElementById('wind-speed');
+const humidity = document.getElementById('humidity');
+const pressure = document.getElementById('pressure');
+const visibility = document.getElementById('visibility');
+const sunrise = document.getElementById('sunrise');
+const sunset = document.getElementById('sunset');
+const time = document.getElementById('timezone');
+
+const notFound = document.getElementById('not-found');
+const info = document.getElementById('info');
+
+searchButton.addEventListener('click', async () => {
+    const apiKey = 'your-api-key';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${givenCity.value}&appid=${apiKey}`;
+
+    if (givenCity.value === "") {
+        alert("Please enter a city name");
+    } else {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (data.cod === "404") {
+                notFound.style.display = "block";
+                info.style.display = "none";
+            } else {
+                country.innerText = data.name + ", " + data.sys.country;
+                temp.innerText = Math.floor(data.main.temp - 273.15) + "°C";
+                weather.innerText = data.weather[0].main;
+                wind.innerText = data.wind.speed + "m/s";
+                humidity.innerText = data.main.humidity + "%";
+                pressure.innerText = data.main.pressure + "hPa";
+                visibility.innerText = data.visibility + "m";
+                sunrise.innerText = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+                sunset.innerText = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+                time.innerText = new Date(data.dt * 1000).toLocaleTimeString();
+                info.style.display = "block";
+                notFound.style.display = "none";
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+});
